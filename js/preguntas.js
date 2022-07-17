4a7f7b814469e49cd6ce177eb9e8fe7e9c354261
jhon <jhonm9696@gmail.com>
2022-07-17 17:47:44 -0500

    avance java Script, pendiente terminar de documentar y corregir la tabla de puntajes

---
+++ js/preguntas.js
@@ -0,0 +1,39 @@
+export let pr1=["Quito es una ciudad de:","Colombia","Venezuela","Ecuador","Argentina","a"];
+export let pr2=["Cual de los siguientes es un IDE","Visual Estudio","NetBeans","GitHub","Notion","b" ]
+export let pr3=["Cual de los Siguientes es un Juego de Zombies","Drive","Resident Evil","Minecraft","Max Payne","b"]
+export let pr4=["Cual de los Siguientes es una cancion de Queen","Where is my Mind","Life is Life","Radio Ga Ga","Losing my Religion","c"]
+export let pr5=["Cuál es el planeta más grande del Sistema Solar","Pluton","Marte","Venus","Jupiter","d"]
+export let categoria1=[pr1,pr2,pr3,pr4,pr5];
+
+export let pr2_1=["La palabra Patojo es originaria de: ","Guatemala","Brasil",  "Peru",  "Chile","a"];
+export let pr2_2=["Cuál es el río más largo del mundo","Tamesis", "Jordan","Amazonas","Magdalena","c"]
+export let pr2_3=["Jerry West es un:","Footbolista","Jugador de baloncesto","Cientifico","Congresista estadounidense","b"]
+export let pr2_4=["A que unidad pertenecio El capitan Price","Escuadron Ghost","Alcones","Fuerza Operativa 141","Join Task Force 2","c"]
+export let pr2_5=["Cual es la Capital de Brasil","Porto Alegre","Brasilia","Rio de Janeiro","Sao Pablo","b"]
+export let categoria2=[pr2_1,pr2_2,pr2_3,pr2_4,pr2_5];
+
+export let pr3_1=["Cual de los siguientes es la composicion quimica de la sal de cocina","AgNO3","NaCl","C12H22O11","HNO3","b"];
+export let pr3_2=["Cuál es el animal más grande de la Tierra","Elefante","Ballena Azul","Jirafa", "Cachalote","b"]
+export let pr3_3=["Cuántos huesos tiene el cuerpo humano","126 huesos","200 huesos","216 huesos","206 huesos","d"]
+export let pr3_4=["¿Cuál es la videoconsola más vendida de la historia?","PlayStation 2","Nintendo Wii","Xbox360","Nintendo64","a"]
+export let pr3_5=["Metal mas pesado de la tabla periodica","Mercurio","Platino","Osmio","Plomo","c"]
+export let categoria3=[pr3_1,pr3_2,pr3_3,pr3_4,pr3_5];
+
+export let pr4_1=["Cuál es el ave más rápida del mundo","Halcón Collarejo","Condor","Aguila Calva", "Halcón Peregrino","a"];
+export let pr4_2=["Quién era el dios vikingo de la luz y de la verdad","Thor","Andhrimnir","Balder","Forseti","c"]
+export let pr4_3=["En qué año el ser humano llegó al espacio","1969","1975","1961","1981","c"]
+export let pr4_4=["Cuál es el deporte más practicado del mundo","Atletismo","Natación", "Basketball","Footboll","b"]
+export let pr4_5=["¿Cuál es el idioma más antiguo en Europa?","Vasco","Italiano","Mandarin","Ingles","a"]
+export let categoria4=[pr4_1,pr4_2,pr4_3,pr4_4,pr4_5];
+
+export let pr5_1=["¿qué es una aféresis?","Es una expresión breve que resume una observación general o un principio moral.", 
+    "Es la repetición del mismo sonido consonante en secuencia.","Es la supresión de una o más letras al principio de un vocablo."
+    ,"Es la acentuacion al final de las palabras","b"];
+export let pr5_2=["¿Quién inventó la lata para conservar alimentos?","Jorge Polansky","Lewis Collins","Pedro Durand","Millicent Roberts","c"]
+export let pr5_3=["Cual es el verdadero nombre del fusil de asalto conocido como Ak-47?","mihail Kalashnikov","Svv kalashnikova","makarova kalashnikov","Avtomat Kalashnikova","d"]
+export let pr5_4=["¿Qué edad tiene el Universo?","13.800 millones de años","10.506 millones de años","18.320 millones de años","23.420 millones de años","a"]
+export let pr5_5=["en que año fue producido el fusil de asalto Ak-47 ","1947","1850","1956","1960","a"]
+export let categoria5=[pr5_1,pr5_2,pr5_3,pr5_4,pr5_5]
+
+export let categorias=[categoria1,categoria2,categoria3,categoria4,categoria5]
+export let tabla_puntuaciones=[]
